9dae2ad91a92bc068a64ceff2bb0ecd902670136
iMplode nZ <rg@youxplode.com>
2021-09-27 20:11:56 -0700

    Add example in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <GotoAndComefrom.h>
 #include <iostream>
 
-int main() {
-	Init_Variables;
+int fact(int x) {
+	Init_Variables
 	Start(x)
-		std::cout << "Hi!";
+		int result = 1;
+		Comefrom_String(["not_finished"], whatever_label)
+		result *= x;
+		x--;
+		if (x <= 0) { Goto(finish) }
+		Define_Pos(not_finished)
+		Define_Pos(finish)
+		return result;
 	End
 }
